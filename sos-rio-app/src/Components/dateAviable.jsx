a86a27ae1c8e46c6ca051dc93bc7abd37d856a6e
Jakiro12 <eclipsisnocte@gmail.com>
2023-01-08 17:28:55 -0300

    forma del ticket

--- sos-rio-app/src/Components/dateAviable.jsx
+++ sos-rio-app/src/Components/dateAviable.jsx
@@ -8,9 +8,26 @@ export default function TicketDay(){
             <main className='ticket-info'>
                 <h3 className='terminal'>NTF</h3>
                 <div className='info-display'>
-                    <p>Puerto Parador</p>
+                    <div className='head-info'>
+                    <h2>Puerto <p>Parador</p></h2>
+                    <p>Pase unico</p>
+                    <p>nombre y apellido</p>
+                    </div>
+                    <div className='day-info'>
+                    <div>
+                    <p>diaria</p>
+                    <button>cambiar</button>
+                    <p>quinquenal</p>
+                    </div>
+                    </div>
+                    <div className='port-info'>
+                    <p>Belgrano 341, Rosario</p>
+                    <p>lunes a domingos (8hs - 18 hs)</p>
+                    </div>
                 </div>
-                <div className='price-station'></div>
+                <div className='price-station'>
+                    <div>hola</div>
+                </div>
             </main>
             <footer className='bottom-decoration'>
                 <div></div>
