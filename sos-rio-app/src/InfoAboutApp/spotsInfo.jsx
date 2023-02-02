4847df8faebb61cf6637002bc6cad04e973dc82a
Jakiro12 <eclipsisnocte@gmail.com>
2023-02-02 13:06:24 -0300

    done it

--- sos-rio-app/src/InfoAboutApp/spotsInfo.jsx
+++ sos-rio-app/src/InfoAboutApp/spotsInfo.jsx
@@ -7,8 +7,17 @@ export default function InfoAboutSpots(){
             <div className="image-content-spot">
                 <img alt="foto de la playa" src={require('../svgLogos/playafondo.jpeg')} />
             </div>
-            <article>
-                informacion de los paradores
+            <article className="spots-travel">
+                <h2>Viaje a las Islas</h2>
+                <p><b>Garden Island</b>:
+                    Situado en la costa de la playa, con apertura de viernes a domingo.
+                    Playa artifical sobre el río con comodidades para disfrutar al aire libre, diversos espacios comunes y amenities de primer nivel.
+                </p>
+                <p><b>El Pimpollal</b>:
+                    El parador cuenta con balneario, salón para eventos, juegos recreativos y excursiones.
+                    El valor de la estadia es de 600$ por persona
+                </p>
+                <p>Los Marinos</p>
             </article>
         </div>
     )
--- sos-rio-app/src/Pages/Menu.jsx
+++ sos-rio-app/src/Pages/Menu.jsx
@@ -2,10 +2,13 @@ import '../StylesComponents/menuOptionsStyles.css'
 import { useNavigate } from 'react-router-dom'
 export default function MenuOption(){
     const navigate=useNavigate()
+    const openBarWeb=()=>{
+        window.open('https://www.instagram.com/elbar.ntf/','_blank')
+    }
     return(<article className='short-info'>
         <div className='container-menu'>
             <button className='tickets-m-btn' onClick={()=>navigate('/shipping')}>Viajes y excursiones</button>
-            <button className='food-m-btn'>Gastronomia del Puerto</button>
+            <button className='food-m-btn' onClick={()=>openBarWeb()}>Gastronomia del Puerto</button>
             <button className='emergency-m-btn'>Numeros de Emergencia y Auxilio</button>
             <button className='promos-m-btn'>Promociones de temporada</button>
 
