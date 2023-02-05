c98bf655062d3e0138927565a6901201ca3760f1
Jakiro12 <eclipsisnocte@gmail.com>
2023-02-05 17:05:23 -0300

    copiar tel funciona

--- sos-rio-app/src/Components/numberEmergency/callHelp.jsx
+++ sos-rio-app/src/Components/numberEmergency/callHelp.jsx
@@ -1,10 +1,44 @@
 import '../../StylesComponents/menuStyles/callHelpersStyles.css'
+
 export default function CallHelpers(){
+   async function copiar(){
+      let textToCopy=document.querySelector('.number-link').textContent
+     await navigator.clipboard.writeText(textToCopy)
+      console.log(textToCopy)
+   }
     return(
-    <div className='number-container'>
-        <div className='prefectura'>prefectura naval
-           <a href='tel:3424420130' >llamar</a>
+    <article className='number-container'>
+        <main className='number-types'>
+        <div className='categorie-phone'>
+          <p className='name-phone'>Prefectura</p>
+          <div className='info-number'> 
+            <a href='tel:3424420130' className='number-link' >Escribir telefono</a>
+           <button className='copy-number' onClick={copiar}>C</button>
+           </div>
         </div>
-    </div>
+        <div className='categorie-phone'>
+          <p className='name-phone'>Prefectura</p>
+          <div className='info-number'> 
+            <a href='tel:3424420130' className='number-link' >3424420130</a>
+            <button className='copy-number' onClick={copiar}>C</button>
+           </div>
+        </div>
+        <div className='categorie-phone'>
+          <p className='name-phone'>Prefectura</p>
+          <div className='info-number'> 
+            <a href='tel:3424420130' className='number-link' >3424420130</a>
+            <button className='copy-number' onClick={copiar}>C</button>
+           </div>
+        </div>
+        <div className='categorie-phone'>
+          <p className='name-phone'>Prefectura</p>
+          <div className='info-number'> 
+            <a href='tel:3424420130' className='number-link' >3424420130</a>
+            <button className='copy-number' onClick={copiar}>C</button>
+           </div>
+        </div>
+    
+        </main>
+    </article>
     )
 }
