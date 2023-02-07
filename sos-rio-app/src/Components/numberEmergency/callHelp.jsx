57be6bcdb1e658d26c5ad29ed311e37f7aeddd37
Jakiro12 <eclipsisnocte@gmail.com>
2023-02-07 11:37:38 -0300

    mejorar cosas

--- sos-rio-app/src/Components/numberEmergency/callHelp.jsx
+++ sos-rio-app/src/Components/numberEmergency/callHelp.jsx
@@ -1,9 +1,11 @@
 import '../../StylesComponents/menuStyles/callHelpersStyles.css'
 
 export default function CallHelpers(){
-   async function copiar(){
+  const allBtn=document.querySelectorAll('.copy-number')
+   async function copyNumberToCall(){
       let textToCopy=document.querySelector('.number-link').textContent
      await navigator.clipboard.writeText(textToCopy)
+    
    }
     return(
     <article className='number-container'>
@@ -12,28 +14,28 @@ export default function CallHelpers(){
           <p className='name-phone'>Prefectura</p>
           <div className='info-number'> 
             <a href='tel:911' className='number-link' >Escribir telefono</a>
-           <button className='copy-number' onClick={copiar}></button>
+           <button className='copy-number' onClick={copyNumberToCall}></button>
            </div>
         </div>
         <div className='categorie-phone'>
           <p className='name-phone'>Prefectura</p>
           <div className='info-number'> 
             <a href='tel:3424420130' className='number-link' >3424420130</a>
-            <button className='copy-number' onClick={copiar}></button>
+            <button className='copy-number' onClick={copyNumberToCall}></button>
            </div>
         </div>
         <div className='categorie-phone'>
           <p className='name-phone'>Prefectura</p>
           <div className='info-number'> 
             <a href='tel:3424420130' className='number-link' >3424420130</a>
-            <button className='copy-number' onClick={copiar}></button>
+            <button className='copy-number' onClick={copyNumberToCall}></button>
            </div>
         </div>
         <div className='categorie-phone'>
           <p className='name-phone'>Prefectura</p>
           <div className='info-number'> 
             <a href='tel:3424420130' className='number-link' >3424420130</a>
-            <button className='copy-number' onClick={copiar}></button>
+            <button className='copy-number' onClick={copyNumberToCall}></button>
            </div>
         </div>
     
