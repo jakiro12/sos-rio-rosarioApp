8e2c9456ca5249f612cb147e6e0bd806dc8226c9
Jakiro12 <eclipsisnocte@gmail.com>
2023-02-21 12:12:57 -0300

    service bar fixed

--- sos-rio-app/src/InfoAboutApp/services.jsx
+++ sos-rio-app/src/InfoAboutApp/services.jsx
@@ -14,7 +14,7 @@ export default function ServiceOfPort(){
                 La ciudad de Rosario se encuentra en la costa del río Paraná, por lo que hay una amplia variedad de opciones para los viajes en bote. 
                 Desde paseos en barcos pequeños para ver la vida silvestre y los paisajes naturales hasta excursiones en barcos más grandes con comodidades como comidas y bebidas, hay algo para todos los gustos.
                 </p>
-                <ul>
+                <ul className="option-boats">
                 <li>Banquito San Andrés <a className="info-boats" href="https://enapro.com.ar/#!/-informacion-ntf/" rel="noreferrer" target="_blank">(mas info)</a></li>
                 <li>Tracker Garden I</li>
                 <li>Gomon Max II</li>
